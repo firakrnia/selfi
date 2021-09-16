cecad578d64356bad31fe56abe0bf4f2aedd7252
firakrnia <fpira13@gmail.com>
2021-09-16 20:17:43 +0700

    field baru table siswa

--- models/user.js
+++ models/user.js
@@ -4,9 +4,15 @@ const database = require("../config/database");
 const User = database.define (
     "siswa",
     {
-        nis : { type: Sequelize.INTEGER},
+        nis : { type: Sequelize.INTEGER, primaryKey: true},
+        nama : {type: Sequelize.STRING(100)},
+        kelas : {type: Sequelize.STRING(5)},
+        jurusan : {type: Sequelize.STRING(20)},
+        nohp : {type: Sequelize.INTEGER(15)},
         password: { type: Sequelize.STRING},
-    }
+
+    },
+    {freezeTableName : true}
 );
 
 User.sync({});
