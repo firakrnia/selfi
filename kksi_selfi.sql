ed74b5617bc3f88e5202c4b678010dab4610f175
firakrnia <fpira13@gmail.com>
2021-09-24 11:07:15 +0700

    db

--- kksi_selfi.sql
+++ kksi_selfi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 17 Sep 2021 pada 04.59
+-- Waktu pembuatan: 24 Sep 2021 pada 06.06
 -- Versi server: 10.4.20-MariaDB
 -- Versi PHP: 8.0.8
 
@@ -28,11 +28,118 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `admin` (
-  `hp_guru` int(15) NOT NULL,
+  `id_guru` int(11) NOT NULL,
+  `hp_guru` varchar(15) DEFAULT NULL,
   `nama_guru` varchar(50) DEFAULT NULL,
-  `keahlian` varchar(25) DEFAULT NULL,
+  `username` varchar(25) DEFAULT NULL,
+  `password` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `buku`
+--
+
+CREATE TABLE `buku` (
+  `id_buku` int(11) NOT NULL,
+  `id_guru` int(15) DEFAULT NULL,
+  `judul_buku` varchar(50) DEFAULT NULL,
+  `deskripsi_buku` varchar(50) DEFAULT NULL,
+  `penulis_buku` varchar(50) DEFAULT NULL,
+  `kategori_buku` varchar(50) DEFAULT NULL,
+  `sampul_buku` varchar(255) DEFAULT NULL,
+  `lampiran_buku` blob DEFAULT NULL,
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jadwal`
+--
+
+CREATE TABLE `jadwal` (
+  `id` int(11) NOT NULL,
+  `id_kelas` int(11) DEFAULT NULL,
+  `hari` varchar(100) DEFAULT NULL,
+  `id_mapel` int(100) DEFAULT NULL,
+  `waktu_dimulai` time DEFAULT NULL,
+  `waktu_selesai` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kelas`
+--
+
+CREATE TABLE `kelas` (
+  `id_kelas` int(11) NOT NULL,
+  `nama` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `kelas`
+--
+
+INSERT INTO `kelas` (`id_kelas`, `nama`) VALUES
+(1, 'X'),
+(2, 'XI'),
+(3, 'XII');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `konseling`
+--
+
+CREATE TABLE `konseling` (
+  `id_konseling` int(11) NOT NULL,
+  `id_guru` int(15) DEFAULT NULL,
+  `jenis_konseling` varchar(25) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mapel`
+--
+
+CREATE TABLE `mapel` (
+  `id_mapel` int(11) NOT NULL,
+  `nama_mapel` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `motivasi`
+--
+
+CREATE TABLE `motivasi` (
+  `id_motivasi` int(11) NOT NULL,
+  `id_guru` int(15) DEFAULT NULL,
+  `judul_artikel` varchar(100) DEFAULT NULL,
+  `deskripsi_artikel` varchar(255) DEFAULT NULL,
+  `kategori_artikel` varchar(255) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `perpustakaan`
+--
+
+CREATE TABLE `perpustakaan` (
+  `id` int(11) NOT NULL,
+  `id_buku` int(15) DEFAULT NULL,
+  `nis` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -44,21 +151,46 @@ CREATE TABLE `admin` (
 CREATE TABLE `siswa` (
   `nis` int(11) NOT NULL,
   `nama` varchar(100) DEFAULT NULL,
-  `kelas` varchar(5) DEFAULT NULL,
+  `id_kelas` int(5) DEFAULT NULL,
   `jurusan` varchar(20) DEFAULT NULL,
-  `nohp` int(15) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `createdAt` datetime NOT NULL,
-  `updatedAt` datetime NOT NULL
+  `nohp` varchar(15) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `siswa`
 --
 
-INSERT INTO `siswa` (`nis`, `nama`, `kelas`, `jurusan`, `nohp`, `password`, `createdAt`, `updatedAt`) VALUES
-(1913950, 'fira', 'XII', 'SIJA', 2147483647, '12345', '2021-09-16 13:12:53', '2021-09-16 13:12:53'),
-(1913951, 'fira', 'XI', 'SIJA', 2147483647, '12345', '2021-09-17 02:24:51', '2021-09-17 02:24:51');
+INSERT INTO `siswa` (`nis`, `nama`, `id_kelas`, `jurusan`, `nohp`, `password`) VALUES
+(1913949, 'fira', 1, 'SIJA', '081282573531', '12345'),
+(1913950, 'fira kurnia putri', 2, 'SIJA', '081282573531', '123456'),
+(1913954, 'firaa', 3, 'SIJA', '081282573521', '123456');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `target`
+--
+
+CREATE TABLE `target` (
+  `id_target` int(11) NOT NULL,
+  `nis` int(11) DEFAULT NULL,
+  `judul_target` varchar(100) DEFAULT NULL,
+  `deskripsi_target` varchar(5) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `todolist`
+--
+
+CREATE TABLE `todolist` (
+  `id_kegiatan` int(11) NOT NULL,
+  `nis` int(11) DEFAULT NULL,
+  `tanggal` datetime DEFAULT NULL,
+  `jam` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
@@ -68,13 +200,195 @@ INSERT INTO `siswa` (`nis`, `nama`, `kelas`, `jurusan`, `nohp`, `password`, `cre
 -- Indeks untuk tabel `admin`
 --
 ALTER TABLE `admin`
-  ADD PRIMARY KEY (`hp_guru`);
+  ADD PRIMARY KEY (`id_guru`);
+
+--
+-- Indeks untuk tabel `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id_buku`),
+  ADD KEY `id_guru` (`id_guru`);
+
+--
+-- Indeks untuk tabel `jadwal`
+--
+ALTER TABLE `jadwal`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_kelas` (`id_kelas`),
+  ADD KEY `id_mapel` (`id_mapel`);
+
+--
+-- Indeks untuk tabel `kelas`
+--
+ALTER TABLE `kelas`
+  ADD PRIMARY KEY (`id_kelas`);
+
+--
+-- Indeks untuk tabel `konseling`
+--
+ALTER TABLE `konseling`
+  ADD PRIMARY KEY (`id_konseling`),
+  ADD KEY `id_guru` (`id_guru`);
+
+--
+-- Indeks untuk tabel `mapel`
+--
+ALTER TABLE `mapel`
+  ADD PRIMARY KEY (`id_mapel`);
+
+--
+-- Indeks untuk tabel `motivasi`
+--
+ALTER TABLE `motivasi`
+  ADD PRIMARY KEY (`id_motivasi`),
+  ADD KEY `id_guru` (`id_guru`);
+
+--
+-- Indeks untuk tabel `perpustakaan`
+--
+ALTER TABLE `perpustakaan`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_buku` (`id_buku`),
+  ADD KEY `nis` (`nis`);
 
 --
 -- Indeks untuk tabel `siswa`
 --
 ALTER TABLE `siswa`
-  ADD PRIMARY KEY (`nis`);
+  ADD PRIMARY KEY (`nis`),
+  ADD KEY `id_kelas` (`id_kelas`);
+
+--
+-- Indeks untuk tabel `target`
+--
+ALTER TABLE `target`
+  ADD PRIMARY KEY (`id_target`),
+  ADD KEY `nis` (`nis`);
+
+--
+-- Indeks untuk tabel `todolist`
+--
+ALTER TABLE `todolist`
+  ADD PRIMARY KEY (`id_kegiatan`),
+  ADD KEY `nis` (`nis`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_guru` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `buku`
+--
+ALTER TABLE `buku`
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `jadwal`
+--
+ALTER TABLE `jadwal`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `kelas`
+--
+ALTER TABLE `kelas`
+  MODIFY `id_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `konseling`
+--
+ALTER TABLE `konseling`
+  MODIFY `id_konseling` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `mapel`
+--
+ALTER TABLE `mapel`
+  MODIFY `id_mapel` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `motivasi`
+--
+ALTER TABLE `motivasi`
+  MODIFY `id_motivasi` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `perpustakaan`
+--
+ALTER TABLE `perpustakaan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `target`
+--
+ALTER TABLE `target`
+  MODIFY `id_target` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `todolist`
+--
+ALTER TABLE `todolist`
+  MODIFY `id_kegiatan` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `buku`
+--
+ALTER TABLE `buku`
+  ADD CONSTRAINT `buku_ibfk_1` FOREIGN KEY (`id_guru`) REFERENCES `admin` (`id_guru`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `jadwal`
+--
+ALTER TABLE `jadwal`
+  ADD CONSTRAINT `jadwal_ibfk_1` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `jadwal_ibfk_2` FOREIGN KEY (`id_mapel`) REFERENCES `mapel` (`id_mapel`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `konseling`
+--
+ALTER TABLE `konseling`
+  ADD CONSTRAINT `konseling_ibfk_1` FOREIGN KEY (`id_guru`) REFERENCES `admin` (`id_guru`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `motivasi`
+--
+ALTER TABLE `motivasi`
+  ADD CONSTRAINT `motivasi_ibfk_1` FOREIGN KEY (`id_guru`) REFERENCES `admin` (`id_guru`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `perpustakaan`
+--
+ALTER TABLE `perpustakaan`
+  ADD CONSTRAINT `perpustakaan_ibfk_1` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id_buku`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `perpustakaan_ibfk_2` FOREIGN KEY (`nis`) REFERENCES `siswa` (`nis`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `siswa`
+--
+ALTER TABLE `siswa`
+  ADD CONSTRAINT `siswa_ibfk_1` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `target`
+--
+ALTER TABLE `target`
+  ADD CONSTRAINT `target_ibfk_1` FOREIGN KEY (`nis`) REFERENCES `siswa` (`nis`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `todolist`
+--
+ALTER TABLE `todolist`
+  ADD CONSTRAINT `todolist_ibfk_1` FOREIGN KEY (`nis`) REFERENCES `siswa` (`nis`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
