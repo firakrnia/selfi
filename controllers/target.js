ce65d92ce6e28d525b8d26fc7ac261e47d84bc8e
firakrnia <fpira13@gmail.com>
2021-10-01 20:51:27 +0700

    update fitur target

--- controllers/target.js
+++ controllers/target.js
@@ -3,6 +3,29 @@ const controller = {};
 
 controller.getAllTarget = async function (req, res) {
     try {
+        let target = await model.target.findAll()
+        if (target.length > 0) {
+            res.status(200).json({
+                success: true,
+                message: "GET Method Target",
+                data: target
+            });
+        } else {
+            res.status(200).json({
+                success: false,
+                message: "Tidak ada data",
+                data: []
+            });
+        }
+    } catch (error) {
+        res.status(404).json({
+            message: error.message
+        });
+    }
+}
+
+controller.getAllTargetByNis = async function (req, res) {
+    try {
         let target = await model.target.findAll({
             where: {
                 nis: req.params.nis
--- routes/target.js
+++ routes/target.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/index');
 
-router.get('/target/:nis', controller.target.getAllTarget);
+router.get('/target', controller.target.getAllTarget);
+
+router.get('/target/:nis', controller.target.getAllTargetByNis);
 
 router.post('/target/tambah', controller.target.post);
 
