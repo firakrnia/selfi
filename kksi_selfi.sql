27f6fb7190bff73ba93294194b5351dec2bb17ad
firakrnia <fpira13@gmail.com>
2021-10-01 14:51:18 +0700

    db terbaru

--- kksi_selfi.sql
+++ kksi_selfi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 24 Sep 2021 pada 06.06
+-- Waktu pembuatan: 01 Okt 2021 pada 09.50
 -- Versi server: 10.4.20-MariaDB
 -- Versi PHP: 8.0.8
 
@@ -49,9 +49,7 @@ CREATE TABLE `buku` (
   `penulis_buku` varchar(50) DEFAULT NULL,
   `kategori_buku` varchar(50) DEFAULT NULL,
   `sampul_buku` varchar(255) DEFAULT NULL,
-  `lampiran_buku` blob DEFAULT NULL,
-  `createdAt` datetime NOT NULL,
-  `updatedAt` datetime NOT NULL
+  `lampiran_buku` blob DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -68,6 +66,25 @@ CREATE TABLE `jadwal` (
   `waktu_dimulai` time DEFAULT NULL,
   `waktu_selesai` time DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `jadwal`
+--
+
+INSERT INTO `jadwal` (`id`, `id_kelas`, `hari`, `id_mapel`, `waktu_dimulai`, `waktu_selesai`) VALUES
+(1, 3, 'Senin', 17, '06:30:00', '08:10:00'),
+(2, 3, 'Senin', 19, '08:25:00', '11:25:00'),
+(3, 3, 'Selasa', 21, '06:30:00', '07:20:00'),
+(4, 3, 'Selasa', 3, '07:20:00', '09:15:00'),
+(5, 3, 'Selasa', 5, '09:15:00', '10:35:00'),
+(6, 3, 'Rabu', 18, '06:30:00', '08:50:00'),
+(7, 3, 'Rabu', 22, '08:50:00', '11:25:00'),
+(8, 3, 'Kamis', 1, '06:30:00', '08:10:00'),
+(9, 3, 'Kamis', 2, '08:25:00', '09:15:00'),
+(10, 3, 'Kamis', 4, '09:15:00', '11:00:00'),
+(11, 3, 'Kamis', 21, '11:00:00', '11:25:00'),
+(12, 3, 'Jumat', 21, '06:30:00', '08:50:00'),
+(13, 3, 'Jumat', 20, '08:50:00', '11:25:00');
 
 -- --------------------------------------------------------
 
@@ -98,9 +115,7 @@ INSERT INTO `kelas` (`id_kelas`, `nama`) VALUES
 CREATE TABLE `konseling` (
   `id_konseling` int(11) NOT NULL,
   `id_guru` int(15) DEFAULT NULL,
-  `jenis_konseling` varchar(25) DEFAULT NULL,
-  `createdAt` datetime NOT NULL,
-  `updatedAt` datetime NOT NULL
+  `jenis_konseling` varchar(25) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -114,6 +129,34 @@ CREATE TABLE `mapel` (
   `nama_mapel` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data untuk tabel `mapel`
+--
+
+INSERT INTO `mapel` (`id_mapel`, `nama_mapel`) VALUES
+(1, 'Matematika'),
+(2, 'Bahasa Indonesia'),
+(3, 'Bahasa Inggris'),
+(4, 'Pend Agama'),
+(5, 'PKN'),
+(6, 'Sejarah Indonesia'),
+(7, 'Bahasa Jepang'),
+(8, 'Seni Budaya'),
+(9, 'Pendidikan Jasmani, Olah Raga dan Kesehatan'),
+(10, 'Simulasi dan Komunikasi Digital'),
+(11, 'Fisika '),
+(12, 'Kimia'),
+(13, 'Sistem Komputer'),
+(14, 'Komputer dan Jaringan Dasar'),
+(15, 'Pemrograman Dasar'),
+(16, 'Dasar Desain Grafis'),
+(17, 'Sistem Keamanan Jaringan'),
+(18, 'Infrastruktur  Komputasi Awan (IaaS)'),
+(19, 'Platform Komputasi Awan (PaaS)'),
+(20, 'Sistem Internet of Things (SIoT)'),
+(21, 'Produk Kreatif dan Kewirausahaan'),
+(22, 'Layanan Komputasi Awan (SaaS)');
+
 -- --------------------------------------------------------
 
 --
@@ -125,9 +168,7 @@ CREATE TABLE `motivasi` (
   `id_guru` int(15) DEFAULT NULL,
   `judul_artikel` varchar(100) DEFAULT NULL,
   `deskripsi_artikel` varchar(255) DEFAULT NULL,
-  `kategori_artikel` varchar(255) DEFAULT NULL,
-  `createdAt` datetime NOT NULL,
-  `updatedAt` datetime NOT NULL
+  `sampul_buku` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -162,9 +203,15 @@ CREATE TABLE `siswa` (
 --
 
 INSERT INTO `siswa` (`nis`, `nama`, `id_kelas`, `jurusan`, `nohp`, `password`) VALUES
+(1913913, 'fira', 3, 'SIJA', '081282573531', '$2a$10$tsngDflCtJO/X.T9eYbxKOfWci0ZfloQMCALqemu.dy0GyTBBdTI2'),
 (1913949, 'fira', 1, 'SIJA', '081282573531', '12345'),
 (1913950, 'fira kurnia putri', 2, 'SIJA', '081282573531', '123456'),
-(1913954, 'firaa', 3, 'SIJA', '081282573521', '123456');
+(1913952, 'fira kp', 1, 'SIJA', '081282573521', '123456'),
+(1913954, 'firaa', 3, 'SIJA', '081282573521', '123456'),
+(1913955, 'fira kp', 1, 'SIJA', '081282573521', '123456'),
+(1913960, 'fira', 3, 'SIJA', '081282573531', '$2a$10$llcKpAQCu4Gaoni1K0fSi.BZWvGrlcKt.0Dz8qnWxNfhkNBonRNvy'),
+(1913961, 'fira', 3, 'SIJA', '081282573531', '$2a$10$u4pKUL3SIz7adl32itjAPuAxLDW8o7VU5BqyyomjcCGlGBfKYEApC'),
+(1913980, 'firaaaa', 3, 'SIJA', '081282573531', NULL);
 
 -- --------------------------------------------------------
 
@@ -176,8 +223,16 @@ CREATE TABLE `target` (
   `id_target` int(11) NOT NULL,
   `nis` int(11) DEFAULT NULL,
   `judul_target` varchar(100) DEFAULT NULL,
-  `deskripsi_target` varchar(5) DEFAULT NULL
+  `deskripsi_target` varchar(500) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `target`
+--
+
+INSERT INTO `target` (`id_target`, `nis`, `judul_target`, `deskripsi_target`) VALUES
+(1, 1913950, 'juara kksi smartschool', 'semangat pasti menang!'),
+(2, 1913961, 'belajar backend', 'apa aja');
 
 -- --------------------------------------------------------
 
@@ -187,10 +242,20 @@ CREATE TABLE `target` (
 
 CREATE TABLE `todolist` (
   `id_kegiatan` int(11) NOT NULL,
+  `nama_kegiatan` varchar(255) DEFAULT NULL,
   `nis` int(11) DEFAULT NULL,
-  `tanggal` datetime DEFAULT NULL,
+  `tanggal` date DEFAULT NULL,
   `jam` time DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `todolist`
+--
+
+INSERT INTO `todolist` (`id_kegiatan`, `nama_kegiatan`, `nis`, `tanggal`, `jam`) VALUES
+(3, NULL, 1913949, '2021-09-26', '21:10:00'),
+(4, NULL, 1913949, '2021-09-26', '21:10:00'),
+(6, 'belajar coding', 1913950, '2021-09-28', '15:00:00');
 
 --
 -- Indexes for dumped tables
@@ -292,7 +357,7 @@ ALTER TABLE `buku`
 -- AUTO_INCREMENT untuk tabel `jadwal`
 --
 ALTER TABLE `jadwal`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT untuk tabel `kelas`
@@ -310,7 +375,7 @@ ALTER TABLE `konseling`
 -- AUTO_INCREMENT untuk tabel `mapel`
 --
 ALTER TABLE `mapel`
-  MODIFY `id_mapel` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_mapel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT untuk tabel `motivasi`
@@ -328,13 +393,13 @@ ALTER TABLE `perpustakaan`
 -- AUTO_INCREMENT untuk tabel `target`
 --
 ALTER TABLE `target`
-  MODIFY `id_target` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_target` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `todolist`
 --
 ALTER TABLE `todolist`
-  MODIFY `id_kegiatan` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_kegiatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
